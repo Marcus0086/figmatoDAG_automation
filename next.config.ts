8a067a8cb673250bb209933f192801ec68e99e8b
Marcus0086 <guptamarcus42@gmail.com>
2024-11-07 22:47:32 +0530

    Added env vars

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    BROWSER_VNC_URL: process.env.BROWSER_VNC_URL,
+    LAMBDA_FUNCTION_URL: process.env.LAMBDA_FUNCTION_URL,
+  },
 };
 
 export default nextConfig;
