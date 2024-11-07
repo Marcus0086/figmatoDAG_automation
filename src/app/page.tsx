3ff08c3739362b38410355f32d1debcfc6c20291
Marcus0086 <guptamarcus42@gmail.com>
2024-11-07 23:56:40 +0530

    added title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FigmaFlow from "@/components/figma/figmaFlow";
 import Browser from "@/components/browser";
+
 const Home = () => {
   return (
     <main className="p-4 flex lg:flex-row flex-col items-center gap-4 justify-center h-screen bg-gray-100 dark:bg-gray-900">
