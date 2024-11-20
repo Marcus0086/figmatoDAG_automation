fe316c530cdc2adb4cd7bd7e2858eb5e1fad8417
Marcus0086 <guptamarcus42@gmail.com>
2024-11-20 17:09:48 +0530

    added is Red flash

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const isRedFlash = (r: number, g: number, b: number, a: number) =>
+  r > 150 && g < 80 && b < 80 && a > 128;
+
+export { isRedFlash };
