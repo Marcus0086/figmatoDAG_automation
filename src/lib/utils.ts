58f56f3659bba541ef9006aeeb377bb85aef2535
Marcus0086 <guptamarcus42@gmail.com>
2024-11-21 20:39:46 +0530

    Refactored: changed function location

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-const isRedFlash = (r: number, g: number, b: number, a: number) =>
-  r > 150 && g < 80 && b < 80 && a > 128;
-
-export { isRedFlash };
