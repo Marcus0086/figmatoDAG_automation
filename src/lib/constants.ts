6f7168a60da57d3d769f53d023e50a2698f68108
Marcus0086 <guptamarcus42@gmail.com>
2024-11-21 10:17:01 +0530

    Refactored

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,9 @@
-const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
 
 const BROWSER_VNC_URL = process.env.BROWSER_VNC_URL || "http://127.0.0.1:6080";
-const LAMBDA_FUNCTION_URL = process.env.LAMBDA_FUNCTION_URL || "http://localhost:3000";
+const LAMBDA_FUNCTION_URL =
+  process.env.LAMBDA_FUNCTION_URL || "http://localhost:3000";
 const ENV = process.env.NODE_ENV;
 
 const BROWSER_ARGS = [
@@ -19,4 +21,11 @@ const INTERACTION_TYPES = {
   ON_SCROLL: "scroll",
 };
 
-export { USER_AGENT, BROWSER_ARGS, BROWSER_VNC_URL, ENV, LAMBDA_FUNCTION_URL, INTERACTION_TYPES };
+export {
+  USER_AGENT,
+  BROWSER_ARGS,
+  BROWSER_VNC_URL,
+  ENV,
+  LAMBDA_FUNCTION_URL,
+  INTERACTION_TYPES,
+};
