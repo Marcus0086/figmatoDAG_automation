b2c54f88bf8d3819a5fc7a4db94a8d2dd71afaf7
Marcus0086 <guptamarcus42@gmail.com>
2024-11-28 00:54:39 +0530

    constants used by browser

---
+++ backend/app/browser/constants.py
@@ -0,0 +1,9 @@
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+
+BROWSER_ARGS = [
+    "--enable-webgl",
+    "--use-gl=swiftshader",
+    "--enable-accelerated-2d-canvas",
+    "--disable-blink-features=AutomationControlled",
+    "--disable-web-security",
+]
