0460aeb624409abe5ca58ea1051aff33aad39dba
Marcus0086 <guptamarcus42@gmail.com>
2024-11-28 00:58:09 +0530

    utility module to update scratchpad

---
+++ backend/app/browser/graph/utils.py
@@ -0,0 +1,21 @@
+import re
+
+from langchain_core.messages import SystemMessage
+
+from .state import AgentState
+
+
+def update_scratchpad(state: AgentState):
+    """After a tool is invoked, we want to update
+    the scratchpad so the agent is aware of its previous steps"""
+    old = state.get("scratchpad")
+    if old:
+        txt = old[0].content
+        last_line = txt.rsplit("\n", 1)[-1]
+        step = int(re.match(r"\d+", last_line).group()) + 1
+    else:
+        txt = "Previous action observations:\n"
+        step = 1
+    txt += f"\n{step}. {state['prediction']['rationale']}"
+
+    return {**state, "scratchpad": [SystemMessage(content=txt)]}
