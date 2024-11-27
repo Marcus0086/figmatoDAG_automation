464576101cdf73d5582ca610dbce8db245e6239d
Marcus0086 <guptamarcus42@gmail.com>
2024-11-28 00:59:38 +0530

    uility module for browser module to format and annotate the page

---
+++ backend/app/browser/utils.py
@@ -0,0 +1,17 @@
+from .service.mark_page import mark_page
+
+
+async def annotate(state):
+    marked_page = await mark_page.with_retry().ainvoke(state["page"])
+    return {**state, **marked_page}
+
+
+def format_descriptions(state):
+    labels = []
+    for i, bbox in enumerate(state["bboxes"]):
+        text = bbox.get("text") or bbox.get("ariaLabel") or bbox.get("aria-label") or ""
+        text = text.strip()
+        el_type = bbox.get("type")
+        labels.append(f'{i} (<{el_type}/>): "{text}"')
+    bbox_descriptions = "\nValid Bounding Boxes:\n" + "\n".join(labels)
+    return {**state, "bbox_descriptions": bbox_descriptions}
