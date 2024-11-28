69321d6f756b2fb568984ebc2e4745a05a5e9057
Marcus0086 <guptamarcus42@gmail.com>
2024-11-28 14:55:27 +0530

    changed styles to make sure both sides have equal heights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import DebugPanel from "@/components/debugPanel";
 const Home = () => {
   return (
     <main className="flex flex-col gap-6">
-      <div className="flex gap-6 h-screen">
-        <div className="w-1/3">
+      <div className="flex flex-col lg:flex-row gap-6 h-full">
+        <div className="w-full lg:w-1/3">
           <ManualWorkflow />
         </div>
-        <Browser />
+        <div className="w-full lg:flex-1">
+          <Browser />
+        </div>
       </div>
       <DebugPanel />
     </main>
