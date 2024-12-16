f34930f12ae7003c3e46ea9f0747ed0d16cde86c
Marcus0086 <guptamarcus42@gmail.com>
2024-12-16 22:44:03 +0530

    Refactor agent.py to enhance functionality and improve structure
    - Introduced new utility functions for formatting verification and summary inputs.
    - Added support for ranking actions and UX scoring with new prompts and structured outputs.
    - Updated the agent and completion logic to utilize the new formatting functions and structured outputs.
    - Improved code organization by grouping imports and enhancing readability.

--- backend/app/browser/agent.py
+++ backend/app/browser/agent.py
@@ -2,31 +2,55 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 
-
-from .utils import format_descriptions, annotate
-from .graph.state import GoalVerificationResult, Prediction
-from .service.prompt import goal_verification_prompt, agent_prompt, summary_prompt
+from .utils import (
+    format_descriptions,
+    annotate,
+    format_verification_input,
+    format_summary_inputs,
+)
+from .graph.state import GoalVerificationResult, Prediction, Actions, ActionRankings
+from .service.prompt import (
+    goal_verification_prompt,
+    agent_prompt,
+    summary_prompt,
+    ranking_prompt,
+    ux_prompt,
+    ux_score,
+)
 from app.config.config import get_settings
+from .graph.utils import (
+    format_ranking_inputs,
+    format_product_memory,
+)
 
 settings = get_settings()
 
+llm = ChatOpenAI(model="gpt-4o", api_key=settings.openai_api_key, temperature=0)
 
-prompt = agent_prompt
-llm = ChatOpenAI(model="gpt-4o", api_key=settings.openai_api_key)
+ux_llm = llm.with_structured_output(Actions)
+ux_agent = annotate | RunnablePassthrough.assign(
+    actions_to_rank=format_product_memory | ux_prompt | ux_llm
+)
+
+ux_score_llm = RunnablePassthrough.assign(ux_score=ux_score | llm | StrOutputParser())
+
+ranking_llm = llm.with_structured_output(ActionRankings)
+ranking_agent = RunnablePassthrough.assign(
+    action_rankings=format_ranking_inputs | ranking_prompt | ranking_llm
+)
 
 structured_llm = llm.with_structured_output(Prediction)
-agent = annotate | RunnablePassthrough.assign(
-    prediction=format_descriptions | prompt | structured_llm
+agent = RunnablePassthrough.assign(
+    prediction=format_descriptions | agent_prompt | structured_llm
 )
 
-
-structured_llm = llm.with_structured_output(GoalVerificationResult)
-
+verification_llm = llm.with_structured_output(GoalVerificationResult)
 completion_agent = annotate | RunnablePassthrough.assign(
-    goal_verification_result=format_descriptions
+    goal_verification_result=format_verification_input
     | goal_verification_prompt
-    | structured_llm
+    | verification_llm
 )
 
-
-summary_llm = summary_prompt | llm | StrOutputParser()
+summary_llm = RunnablePassthrough.assign(
+    summary=format_summary_inputs | summary_prompt | llm | StrOutputParser()
+)
