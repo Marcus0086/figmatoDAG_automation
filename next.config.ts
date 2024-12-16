20068afb997403f22a7a5a78173ae5dbe1a7195a
Marcus0086 <guptamarcus42@gmail.com>
2024-12-16 22:43:10 +0530

    Added aws creds to allow image upload from frontend

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     LAMBDA_FUNCTION_URL: process.env.LAMBDA_FUNCTION_URL,
     IMAGE_HOST: process.env.IMAGE_HOST,
     BACKEND_URL: process.env.BACKEND_URL,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
   },
   images: {
     remotePatterns: [
